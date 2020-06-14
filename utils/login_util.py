868ca292ab67ce19b06c22dbc63983ed955de73a
Ashm Walia <ashmwalia@outlook.com>
2020-06-14 05:21:39 -0700

    Debiugging

--- utils/login_util.py
+++ utils/login_util.py
@@ -5,16 +5,17 @@ def check_valid_user(username, password):
     try:
         login_result = login(username, password)
     except Exception as e:
-        return {'is_logged_in':False,'user_type':"none"}
+        return {'is_logged_in':False,'fullname':"none"}
 
     if login_result['is_logged_in']:
         return login_result
-    return {'is_logged_in':False,'user_type':"none"}
+    return {'is_logged_in':False,'fullname':"none"}
 
 def login(username, password):
     try:
         logged_in_user = usr.query.filter_by(username=username).first()
+        print(logged_in_user)
     except Exception as e:
         print("Error occured while trying to login : " + str(e))
         raise e
-    return {'is_logged_in': (check_password_hash(logged_in_user.password, password=password)), 'user_type': logged_in_user.user_type}
+    return {'is_logged_in': (check_password_hash(logged_in_user.password, password=password)), 'fullname': logged_in_user.fullname}
