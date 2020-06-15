78705e8f5615c27eb946ed487ac9484bb55910ec
Ashm Walia <ashmwalia@outlook.com>
2020-06-15 11:25:41 -0700

    code refactoring

--- resources/user_api.py
+++ resources/user_api.py
@@ -1,19 +1,19 @@
 from flask_restful import Resource
 from models.user import User
 import flask
-import json
-from utils.login_util import check_valid_user
+from utils.user_util import check_valid_user, load_json, add_user
 
 class UserAPI(Resource):
     def post(self):
-        username = flask.request.form.get('usrname', "")
-        password = flask.request.form.get('password', "")
-        fullname = flask.request.form.get('fullname', "")
-        return(User.add_user(username, password,fullname))
+        params = load_json(flask.request)
+        username = params['username']
+        password = params['password']
+        fullname = params['fullname']
+        return add_user(username, password, fullname)
 
 class LoginAPI(Resource):
     def post(self):
-        params = json.loads(flask.request.data.decode('utf8').replace("'", '"'))
+        params = load_json(flask.request)
         result = check_valid_user(params['username'],params['password'])
         data = flask.jsonify(result)
         if result['is_logged_in']:
--- utils/user_util.py
+++ utils/user_util.py
@@ -1,5 +1,6 @@
 from models.user import User as usr
 from werkzeug.security import check_password_hash
+import json
 
 def check_valid_user(username, password):
     try:
@@ -18,3 +19,9 @@ def login(username, password):
         print("Error occured while trying to login : " + str(e))
         raise e
     return {'is_logged_in': (check_password_hash(logged_in_user.password, password=password)), 'fullname': logged_in_user.fullname}
+
+def load_json(request):
+    return json.loads(request.data.decode('utf8').replace("'", '"'))
+
+def add_user(username, password, fullname):
+    return (usr.add_user(username, password, fullname))
